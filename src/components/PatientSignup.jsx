afbda548b28c5c154d0122fed7b3c177ade2f567
aniketchawardol <aniketchawardol@gmail.com>
2024-09-08 00:09:50 +0530

    added details in patient signup

--- src/components/PatientSignup.jsx
+++ src/components/PatientSignup.jsx
@@ -5,20 +5,23 @@ import "../App.css";
 function PatientSignup() {
   return (
     <>
-<div className='flex bg-teal-100 justify-center h-screen items-center animate-appear'>
-<div className='flex flex-col items-center'>
+<div className='flex bg-teal-100 justify-center h-full items-center animate-appear'>
+<div className='flex flex-col my-3 items-center'>
   <div className='border-2 rounded-3xl text-center bg-white drop-shadow-2xl'>
     <p className='mx-3 my-5 text-xl font-custom2'>Welcome to Doctors.com</p>
     <UploadButton label ="Upload Profile photo"/>
     <input type="text" placeholder='Enter fullname' className='textinput' />
     <input type="text" placeholder='Enter email' className='textinput' />
     <button className='buttons'>Send OTP </button>
-    <input type="text" placeholder='Enter OTP for email' className='smalltextinput' />
+    <input type="number" placeholder='Enter OTP for email' className='smalltextinput' />
     <button className='buttons'>Verify </button>
-    <input type="text" placeholder='Enter phone number' className='textinput' />
+    <input type="number" placeholder='Enter phone number' className='textinput' />
     <button className='buttons'>Send OTP </button>
-    <input type="text" placeholder='Enter OTP for number' className='smalltextinput' />
-    <button className='buttons'>Verify </button>
+    <input type="number" placeholder='Enter OTP for number' className='smalltextinput' />
+    <button className='buttons'>Verify </button><br />
+    <input type="text" placeholder='Enter weight' className='smalltextinput' />
+    <input type="text" placeholder='Enter age' className='smalltextinput' /><br />
+    <input type="text" placeholder='Enter Bloodgroup' className='smalltextinput' />
     <input type="password" placeholder='Enter password' className='textinput' />
     <input type="text" placeholder='REenter password' className='textinput' />
     <UploadButton label ="Upload previous reports"/>
