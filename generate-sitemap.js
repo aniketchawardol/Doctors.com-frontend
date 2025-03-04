3cdef1ee716de5e0ed3ad594797f3b44135db955
Aniket <aniketchawardol@gmail.com>
2025-03-05 00:45:08 +0530

    feat: add sitemap generation script and update build process

---
+++ generate-sitemap.js
@@ -0,0 +1,21 @@
+import { createWriteStream } from "fs";
+import { SitemapStream } from "sitemap";
+
+const sitemap = new SitemapStream({ hostname: "https://doctors-com-frontend.vercel.app/" });
+
+const writeStream = createWriteStream("./dist/sitemap.xml");
+
+// static routes
+const links = [
+  { url: "/", changefreq: "daily", priority: 1.0 },
+  { url: "/login", changefreq: "monthly", priority: 0.7 },
+  { url: "/signup", changefreq: "monthly", priority: 0.7 },
+  { url: "/signup/patientsignup", changefreq: "monthly", priority: 0.6 },
+  { url: "/signup/hospitalsignup", changefreq: "monthly", priority: 0.6 },
+];
+
+
+// Write all links to sitemap
+links.forEach((link) => sitemap.write(link));
+sitemap.end();
+sitemap.pipe(writeStream);
