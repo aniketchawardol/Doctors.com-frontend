65ef6737936c7f930f2ed7275b8cc1ec67ce274c
Aniket <aniketchawardol@gmail.com>
2025-06-16 09:06:12 +0530

    feat: adjust Layout component structure for better footer positioning and layout consistency

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,10 +6,12 @@ function Layout() {
   return (
     <>
       <div className="flex flex-col min-h-screen animate-appear w-full bg-gradient-to-tr from-white from-40% via-amber-100 to-teal-100">
-        <main className="flex-grow">
+        <main className="flex-grow w-full pb-8">
           <Outlet />
         </main>
-        <Footer />
+        <div className="flex-shrink-0">
+          <Footer />
+        </div>
       </div>
     </>
   );
