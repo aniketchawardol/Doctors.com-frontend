985a08c3e1f240cde35b9591965ac79afcdffb1c
Aniket <aniketchawardol@gmail.com>
2025-06-16 08:59:59 +0530

    feat: restructure Layout component for improved layout and semantics

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Footer from './Footer'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 function Layout() {
   return (
     <>
-      <div className='animate-appear h-full w-full bg-gradient-to-tr from-white from-40% via-amber-100 to-teal-100'>
-      <Outlet /> 
-      <Footer /> 
-      </div>  
+      <div className="flex flex-col min-h-screen animate-appear w-full bg-gradient-to-tr from-white from-40% via-amber-100 to-teal-100">
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
